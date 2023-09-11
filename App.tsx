7e9f6335cb4bceda00627430f870261c00f9dfdd
Samy <s_rahim@estin.dz>
2023-09-11 22:41:42 +0100

    init

---
+++ App.tsx
@@ -0,0 +1,29 @@
+import { ClerkProvider } from "@clerk/clerk-expo";
+import { SignIn, SignedIn, SignedOut } from "@clerk/clerk-react";
+import { NavigationContainer } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import SignUpScreen from "./screens/SignUpScreen";
+import SignInScreen from "./screens/SignInScreen";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+
+export default function App() {
+  return (
+    <ClerkProvider
+      publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
+      <SafeAreaProvider>
+        <SafeAreaView>
+          <View className="justify-center h-full items-center text-2xl">
+            <SignedIn>
+              <Text className="bg-black">Welcome to your app!</Text>
+            </SignedIn>
+            <SignedOut>
+              <SignInScreen />
+            </SignedOut>
+          </View>
+        </SafeAreaView>
+      </SafeAreaProvider>
+    </ClerkProvider>
+  );
+}
---
+++ screens/SignUpScreen.tsx
@@ -0,0 +1,100 @@
+import * as React from "react";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
+import { useSignUp } from "@clerk/clerk-expo";
+
+export default function SignUpScreen() {
+  const { isLoaded, signUp, setActive } = useSignUp();
+
+  const [emailAddress, setEmailAddress] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [pendingVerification, setPendingVerification] = React.useState(false);
+  const [code, setCode] = React.useState("");
+
+  // start the sign up process.
+  const onSignUpPress = async () => {
+    if (!isLoaded) {
+      return;
+    }
+
+    try {
+      await signUp.create({
+        emailAddress,
+        password,
+      });
+
+      // send the email.
+      await signUp.prepareEmailAddressVerification({ strategy: "email_code" });
+
+      // change the UI to our pending section.
+      setPendingVerification(true);
+    } catch (err: any) {
+      console.error(JSON.stringify(err, null, 2));
+    }
+  };
+
+  // This verifies the user using email code that is delivered.
+  const onPressVerify = async () => {
+    if (!isLoaded) {
+      return;
+    }
+
+    try {
+      const completeSignUp = await signUp.attemptEmailAddressVerification({
+        code,
+      });
+
+      await setActive({ session: completeSignUp.createdSessionId });
+    } catch (err: any) {
+      console.error(JSON.stringify(err, null, 2));
+    }
+  };
+
+  return (
+    <View className="">
+      {!pendingVerification && (
+        <View>
+          <View className="">
+            <TextInput
+              className="border rounded-xl p-2 mb-2 border-gray-300"
+              autoCapitalize="none"
+              value={emailAddress}
+              placeholder="Email or phone number"
+              onChangeText={(email) => setEmailAddress(email)}
+            />
+          </View>
+
+          <View>
+            <TextInput
+              className="border rounded-xl p-2 mb-2 border-gray-300"
+              value={password}
+              placeholder="Password"
+              secureTextEntry={true}
+              onChangeText={(password) => setPassword(password)}
+            />
+          </View>
+
+          <TouchableOpacity
+            onPress={onSignUpPress}
+            className="bg-orange-500 rounded-xl  p-2 "
+          >
+            <Text className="text-white">Sign up</Text>
+          </TouchableOpacity>
+        </View>
+      )}
+      {pendingVerification && (
+        <View>
+          <View>
+            <TextInput
+              value={code}
+              placeholder="Code..."
+              onChangeText={(code) => setCode(code)}
+            />
+          </View>
+          <TouchableOpacity onPress={onPressVerify}>
+            <Text>Verify Email</Text>
+          </TouchableOpacity>
+        </View>
+      )}
+    </View>
+  );
+}
