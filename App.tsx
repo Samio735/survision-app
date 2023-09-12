636170552182a6a2398b26f7d3ce7310d603d56f
Samy <s_rahim@estin.dz>
2023-09-12 12:37:35 +0100

    add web support

--- App.tsx
+++ App.tsx
@@ -1,29 +1,20 @@
-import { ClerkProvider } from "@clerk/clerk-expo";
-import { SignIn, SignedIn, SignedOut } from "@clerk/clerk-react";
-import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import SignUpScreen from "./screens/SignUpScreen";
-import SignInScreen from "./screens/SignInScreen";
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
   return (
-    <ClerkProvider
-      publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}
-    >
-      <SafeAreaProvider>
-        <SafeAreaView>
-          <View className="justify-center h-full items-center text-2xl">
-            <SignedIn>
-              <Text className="bg">Welcome to your app!</Text>
-            </SignedIn>
-            <SignedOut>
-              <SignInScreen />
-            </SignedOut>
-          </View>
-        </SafeAreaView>
-      </SafeAreaProvider>
-    </ClerkProvider>
+    <View style={styles.container}>
+      <Text className="bg-black">Open up App.js to working on your !</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
